f04592e72a70ded0919b419ab3a425225f6adca9
play4uman <play4uman@gmail.com>
2021-02-08 11:34:48 +0200

    Add project files.

---
+++ customdeploy/customdeploy.cmd
@@ -0,0 +1,3 @@
+cd ..
+winrar a -afzip -r -x*\.vs\ -x*\bin\ -x*\obj\ -x*\customdeploy\ .\customdeploy\code
+az webapp deployment source config-zip --resource-group "TimmyBotRG" --name "timmybot-web" --src ".\customdeploy\code.zip"
